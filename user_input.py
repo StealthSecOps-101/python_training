15e8de03c64ab1c5e5f3519b6175953e5934c4fe
StealthSecOps-101 <160061471+StealthSecOps-101@users.noreply.github.com>
2024-02-28 17:17:57 +0300

    user input file

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input ("what is your name:")
+age = input ("what is your age:")
+location = input ("what is your location:")
+
+print ('hello ',name)
+print ('your age is',age)
+print ('you live in', location)
